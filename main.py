adf0e7568878a92a33549479c8b224fd3aa9af70
Shukolza <shukolza@gmail.com>
2025-03-05 17:26:22 +0600

    Bugfix & Score system rework

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def resource_path(relative_path):
 
 
 with open(resource_path("hiscore.txt"), "r") as file:
-    high_score = int(file.read())
+    high_score = float(file.read())
 
 with open(resource_path("settings.json"), "r") as file:
     settings = json.load(file)
@@ -44,6 +44,16 @@ with open(resource_path("settings.json"), "r") as file:
 def clear():
     """Generate command to clear screen depends on os type"""
     os.system("cls" if os.name == "nt" else "clear")
+
+
+def calculate_score(min_num, max_num, spent_attempts, attempts_amount):
+    """Calculate score depends on game parameters"""
+    range_size = max_num - min_num + 1
+    if attempts_amount != "infinity":
+        score = round((range_size**0.5) / spent_attempts, 2)
+    else:
+        score = round((range_size**0.5) / (spent_attempts * 0.5), 2)
+    return score
 
 
 def settings_menu():
@@ -110,7 +120,7 @@ def game_over(
     print(f"Total attempts: {attempts_amount}")
     print("Your suggestions:")
     tmp_counter = 1
-    for suggestion in len(user_suggestions):
+    for suggestion in range(len(user_suggestions)):
         print(
             f"[{tmp_counter}] {user_suggestions[suggestion]} : {system_answers[suggestion]}"
         )
@@ -126,16 +136,22 @@ def winner(
     correct_random_number: int,
     user_suggestions: list,
     system_answers: list,
+    number_range: list,
 ):
     global high_score
+    score = calculate_score(
+        number_range[0], number_range[1], spent_attempts, attempts_amount
+    )
+
     clear()
     print("============================ YOU WON ============================")
     print(f"Spent attempts: {spent_attempts} / {attempts_amount}")
-    if spent_attempts < high_score:
-        high_score = spent_attempts
+    print(f"Your score: {score}")
+    if score > high_score:
+        high_score = score
         print(f"NEW HIGH SCORE!")
         with open("hiscore.txt", "w") as file:
-            file.write(high_score)
+            file.write(str(high_score))
     print("Your suggestions:")
     tmp_counter = 1
     for suggestion in range(len(user_suggestions)):
@@ -160,6 +176,7 @@ def game():
             settings["random_number_range_1"], settings["random_number_range_2"]
         )
         current_attempts_amount = settings["attempts_count"]
+        attempts_amount_counter = current_attempts_amount
         spent_attempts_counter = 0
         user_suggestions = []
         system_answers = []
@@ -178,7 +195,7 @@ def game():
             print(
                 "============================ Guess a number ============================"
             )
-            print(f"{current_attempts_amount} attempts left.")
+            print(f"{attempts_amount_counter} attempts left.")
             print(f"You spent {spent_attempts_counter} attempts so far.")
             print()
             print("Your last suggestions:", end="\n")
@@ -206,6 +223,10 @@ def game():
                     correct_random_num,
                     user_suggestions,
                     system_answers,
+                    [
+                        settings["random_number_range_1"],
+                        settings["random_number_range_2"],
+                    ],
                 )
                 break
             elif user_suggestion < correct_random_num:
@@ -213,7 +234,7 @@ def game():
                 user_suggestions.append(str(user_suggestion))
                 spent_attempts_counter += 1
                 try:
-                    current_attempts_amount -= 1
+                    attempts_amount_counter -= 1
                 except TypeError:
                     pass
                 print("Must be higher!")
@@ -224,7 +245,7 @@ def game():
                 user_suggestions.append(str(user_suggestion))
                 spent_attempts_counter += 1
                 try:
-                    current_attempts_amount -= 1
+                    attempts_amount_counter -= 1
                 except TypeError:
                     pass
                 print("Must be lower!")
